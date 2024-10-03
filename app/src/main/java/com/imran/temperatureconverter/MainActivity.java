1191ef18010eb42e1069081a928b13d4449576e7
IHC25 <imranhchy@gmail.com>
2024-10-03 21:37:43 +0600

    design & functionality added

--- app/src/main/java/com/imran/temperatureconverter/MainActivity.java
+++ app/src/main/java/com/imran/temperatureconverter/MainActivity.java
@@ -4,7 +4,10 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
@@ -14,7 +17,14 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Declare variables
     Spinner spinnerUnits;
+    EditText edTempValue;
+    TextView result;
+    Button convertButton;
+    boolean isCelsius, isFahrenheit;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,7 +36,11 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
 
+        // Get references to the views
         spinnerUnits = findViewById(R.id.spinnerUnits);
+        edTempValue = findViewById(R.id.edTempValue);
+        result = findViewById(R.id.result);
+        convertButton = findViewById(R.id.convertButton);
 
         // Create an ArrayAdapter using the string array and a default spinner layout
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,R.array.temperature_units, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item);
@@ -34,6 +48,7 @@ public class MainActivity extends AppCompatActivity {
         adapter.setDropDownViewResource(androidx.appcompat.R.layout.support_simple_spinner_dropdown_item);
         // Apply the adapter to the spinner
         spinnerUnits.setAdapter(adapter);
+
 
         // Set up an onItemSelectedListener
         spinnerUnits.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
@@ -43,18 +58,43 @@ public class MainActivity extends AppCompatActivity {
                 String selectedItem = adapterView.getItemAtPosition(i).toString();
                 //Test if it works by a Toast
                 if (selectedItem.equals("Celsius")){
-                    Toast toast = Toast.makeText(getApplicationContext(), "Celsius", Toast.LENGTH_SHORT);
-                    toast.show();
+                    isCelsius = true;
+                    isFahrenheit = false;
                 }
                 else if (selectedItem.equals("Fahrenheit")){
-                    Toast toast = Toast.makeText(getApplicationContext(), "Fahrenheit", Toast.LENGTH_SHORT);
-                    toast.show();
+                    isFahrenheit = true;
+                    isCelsius = false;
                 }
             }
 
             @Override
             public void onNothingSelected(AdapterView<?> adapterView) {
+                System.out.println("Nothing selected");
+            }
+        });
 
+        convertButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //get temperature value
+                String sTempValue = edTempValue.getText().toString();
+                //convert to float
+                float tempValue = 0;
+                //check selected unit
+                if (isCelsius && !isFahrenheit && !sTempValue.isEmpty()){
+                    //convert to fahrenheit
+                    tempValue = Float.parseFloat(sTempValue);
+                    float celsiusToFahrenheit = (tempValue * 9/5) + 32;
+                    result.setText(String.valueOf(celsiusToFahrenheit +" °F"));
+
+                } else if (isFahrenheit && !isCelsius && !sTempValue.isEmpty()){
+                    //convert to celsius
+                    tempValue = Float.parseFloat(sTempValue);
+                    float fahrenheitToCelsius = (tempValue - 32) * 5/9;
+                    result.setText(String.valueOf(fahrenheitToCelsius +" °C"));
+                } else {
+                    Toast.makeText(MainActivity.this, "Please select a unit", Toast.LENGTH_SHORT).show();
+                }
             }
         });
 
